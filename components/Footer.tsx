254c45dc0955aa5a6cdb0580c0da153237851cd5
mgs-chneider <mgs@schneiderundcie.com>
2026-04-18 16:28:25 +0200

    Add Footer component with site content

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,11 @@
+import { siteContent } from "@/content/site"; 
 
+export function Footer() { 
+  return ( 
+    <footer className="site-footer"> 
+      <div className="container footer-inner"> 
+        <p>{siteContent.footerText}</p> 
+      </div> 
+    </footer> 
+  ); 
+}
