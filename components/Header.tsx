4464de995f1b3467981ad52fee31a390bbc12eec
mgs-chneider <mgs@schneiderundcie.com>
2026-04-18 16:26:16 +0200

    Add Header component with navigation links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,1 +1,18 @@
+import Link from "next/link"; 
+import { siteContent } from "@/content/site"; 
 
+export function Header() 
+  { return ( 
+    <header className="site-header"> 
+      <div className="container header-inner"> 
+        <Link href="/" className="site-brand"> 
+          {siteContent.siteTitle} 
+        </Link> 
+        <nav className="site-nav"> 
+          <Link href="/">Galerie</Link>
+          <a href={`mailto:${siteContent.contactEmail}`}>Kontakt</a> 
+        </nav> 
+      </div> 
+    </header> 
+  ); 
+  }
