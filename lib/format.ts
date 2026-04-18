1b02cd99ef51e00cb5fd734b1b197e5074693413
mgs-chneider <mgs@schneiderundcie.com>
2026-04-18 16:12:42 +0200

    Add statusLabels for ObjectStatus translations

--- lib/format.ts
+++ lib/format.ts
@@ -1,1 +1,7 @@
+import { ObjectStatus } from "./types";
 
+export const statusLabels: Record<ObjectStatus, string> = {
+  verfuegbar: "Verfügbar",
+  reserviert: "Reserviert",
+  verkauft: "Verkauft",
+};
