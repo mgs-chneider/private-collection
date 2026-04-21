8f1fe48046efc4f702c2d4f3eaeffc53ba0a1385
mgs-chneider <mgs@schneiderundcie.com>
2026-04-21 13:53:31 +0200

    Add Impressions component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import Preamble from '@/components/Preamble';
 import Collection from '@/components/Collection';
+import Impressions from '@/components/Impressions';
 import Footer from '@/components/Footer';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <Preamble />
       <Collection />
+      <Impressions />
       <Footer />
     </>
   );
