7705cd4ea522e9cdeaded8c6f8f0be5f96512449
mgs-chneider <mgs@schneiderundcie.com>
2026-04-25 17:37:04 +0200

    Add sitemap function for site metadata
    Implement sitemap function to define site URLs with metadata.

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.privat-besitz.de',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.privat-besitz.de/impressum',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    {
+      url: 'https://www.privat-besitz.de/datenschutz',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
