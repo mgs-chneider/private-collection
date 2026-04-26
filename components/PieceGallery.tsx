44700bcc3b1b6cd0bb689d4fc0dd23bf17183c77
mgs-chneider <mgs@schneiderundcie.com>
2026-04-26 12:52:35 +0200

    Add PieceGallery component for image display

---
+++ components/PieceGallery.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+
+interface Props {
+  images: string[];
+  title: string;
+}
+
+export default function PieceGallery({ images, title }: Props) {
+  const [activeIdx, setActiveIdx] = useState(0);
+
+  return (
+    <div className="piece-gallery">
+      <div className="piece-gallery-main">
+        <img
+          src={images[activeIdx]}
+          alt={title}
+          className="piece-gallery-photo"
+        />
+      </div>
+
+      {images.length > 1 && (
+        <div className="modal-thumbs">
+          {images.map((src, i) => (
+            <button
+              key={src}
+              type="button"
+              role="tab"
+              aria-selected={i === activeIdx}
+              className={`modal-thumb${i === activeIdx ? ' is-active' : ''}`}
+              onClick={() => setActiveIdx(i)}
+            >
+              <img src={src} alt="" />
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
